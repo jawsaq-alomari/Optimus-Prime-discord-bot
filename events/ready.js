b10958d1e0ca566db3e8cb9bfd9bd36a3acdb968
jawsaq-alomari <jawsaqo5@gmail.com>
2021-11-25 20:18:41 +0200

    updated status

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 module.exports = async (client) => {
     client.Ready = true, 
     client.user.setPresence({
-      status: "online",  // You can show online, idle, and dnd
+      status: "idle",  // You can show online, idle, and dnd
       activity: {
           name: "your mom getting railed",  // The message shown
           type: "WATCHING", // PLAYING, WATCHING, LISTENING, STREAMING,
